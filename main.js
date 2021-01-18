e39cd390759680d90ca424a8599e703012ac4f73
MDCo <moonshotdesignco@gmail.com>
2021-01-18 14:10:39 -0500

    menu button update

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 document.getElementById("menuBtn").addEventListener("click", function() {
+  this.classList.toggle("rotate");
   document.getElementById("menuList").classList.toggle("menu-closed");
 });
